0ae9873d7f2486969cbbeb83d156bc278c0e0f12
paulinawojtysiakk <paulina.wwojtysiak@gmail.com>
2023-07-11 18:07:08 +0200

    3.2_Notifications v2

---
+++ development/js/notifications.js
@@ -0,0 +1,17 @@
+
+const recipeNotification = document.getElementById("recipeNotification");
+const mealPlanNotification = document.getElementById("mealPlanNotification");
+const welcomeNotification = document.getElementById("welcomeNotification");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+btn1.addEventListener("click", () => {
+    recipeNotification.style.display = "none";
+});
+btn2.addEventListener("click", () => {
+    mealPlanNotification.style.display = "none";
+});
+btn3.addEventListener("click", () => {
+    welcomeNotification.style.display = "none";
+});
+
